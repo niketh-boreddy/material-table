7afada1b70bcc32b9d8da98e0c00d41ac817457d
Niketh Boreddy <niketh.r.boreddy@gmail.com>
2020-10-19 20:15:48 -0700

    Allowing both actions and selection features

--- dist/material-table.js
+++ dist/material-table.js
@@ -669,7 +669,7 @@ function (_React$Component) {
           hasDetailPanel: !!props.detailPanel,
           detailPanelColumnAlignment: props.options.detailPanelColumnAlignment,
           showActionsColumn: props.actions && props.actions.filter(function (a) {
-            return !a.isFreeAction && !_this4.props.options.selection;
+            return !a.isFreeAction;
           }).length > 0,
           showSelectAllCheckbox: props.options.showSelectAllCheckbox,
           orderBy: _this4.state.orderBy,
